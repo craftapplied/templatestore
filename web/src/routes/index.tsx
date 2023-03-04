7cd7242ff7371bc89ff2c6ac7315bc7051df0c5e
Darren Barklie <darrenbarklie@gmail.com>
2023-03-04 12:20:05 +0000

    Update base component, calls to Supabase

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -11,65 +11,34 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_PROJECT_KEY_PUBLIC;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+interface Product {
+  title: string;
+}
+
 export default function Home() {
-  const [products, setProducts] = createSignal();
+  const [products, setProducts] = createSignal<Product[]>([]);
 
   createEffect(() => {
     getProducts();
   });
 
   async function getProducts() {
-    const { data } = await supabase.from("products").select();
-    setProducts(data);
-  }
-
-  const [countries, setCountries] = createSignal();
-
-  createEffect(() => {
-    getCountries();
-  });
-
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
+    const { data } = await supabase.from("products").select("*");
+    setProducts(data as Product[]);
   }
 
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
+        Template Store
       </h1>
+
       <Counter />
 
-      {/* <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p> */}
-
-      {/* {JSON.stringify(supabaseUrl)}
-      {JSON.stringify(supabaseKey)} */}
-      {JSON.stringify(countries)}
-
       <ul>
-        <For each={products()}>{(product) => <li>{product.title}</li>}</For>
-      </ul>
-
-      <ul>
-        <For each={countries()}>{(country) => <li>{country.name}</li>}</For>
+        <For each={products()} fallback={<div>No items</div>}>
+          {(item) => <li>{item.title}</li>}
+        </For>
       </ul>
     </main>
   );
